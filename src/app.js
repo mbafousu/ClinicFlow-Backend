29165058183f25de3b1ab1b38c56f15cbb91c1e0
mbafousu <carolngue06@gmail.com>
2026-03-04 16:11:08 -0500

    Add protected patient CRUD routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import patientRoutes from "./routes/patientRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/patients", patientRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "ClinicFlow API running " });
