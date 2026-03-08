59d5c8b5af30984c78de6d4e73b8697776abb861
mbafousu <carolngue06@gmail.com>
2026-03-08 10:19:56 -0400

    Add asyncHandler utility and improve global error handling

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,22 @@
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
+  // duplicate key error (unique email)
+  if (err.code === 11000) {
+    return res.status(400).json({
+      message: "Duplicate field value",
+      details: err.keyValue,
+    });
+  }
+
+  // validation error
+  if (err.name === "ValidationError") {
+    return res.status(400).json({
+      message: "Validation error",
+      details: err.errors,
+    });
+  }
+
   res.status(err.status || 500).json({
     message: err.message || "Server Error",
   });
